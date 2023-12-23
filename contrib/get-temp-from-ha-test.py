bce9f6e5cffc376b746b46353738a24e7de3d61a
Robin Hack <hack.robin@gmail.com>
2023-12-23 21:24:39 +0100

    contrib: Add typehints.

--- contrib/get-temp-from-ha-test.py
+++ contrib/get-temp-from-ha-test.py
@@ -1,15 +1,23 @@
-from requests import get
 import json
 
-token = ""
-address = ""
+from requests import get
+from typing import Dict
 
-url = f"http://{address}:8123/api/states/sensor.babice_s_temperature"
-headers = {
+token : str = ""
+address : str = ""
+
+base_url : str = f"http://{address}:8123/api/states/"
+headers : Dict[str, str] = {
     "Authorization": f"Bearer {token}",
     "content-type": "application/json",
 }
 
-json_content = json.loads(get(url, headers=headers).text)
-celsius = json_content["state"]
-print(f"Temperature: {celsius}")
+def get_temp(sensor : str) -> float:
+    json_content = json.loads(get(base_url + sensor, headers=headers).text)
+    return json_content["state"]
+
+
+village_celsius : float = get_temp("sensor.babice_s_temperature")
+stadla_celsius : float = get_temp("sensor.netatmo_favourite_weather_stations_nfws_bns_tono_stadla_temperature")
+
+print(f"Temperature is {village_celsius} and {stadla_celsius}")
